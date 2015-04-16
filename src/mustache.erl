e5495e3a27140b6e3b09e469e202d0d463d24f94
Emil Falk <emil.falk@textalk.se.se>
2015-04-16 17:08:53 +0200

    WIP

--- src/mustache.erl
+++ src/mustache.erl
@@ -25,178 +25,39 @@
 
 -module(mustache).
 -author("Tom Preston-Werner").
--export([compile/1, render/2]).
+-export([compile/1]).
 
 -record(state, {
-          this,
-          contexts = #{},
-          section_re,
-          tag_re
+          left = <<"{{({|">>,
+          right = <<"}}">>,
+          path = []
          }).
 
 -include_lib("eunit/include/eunit.hrl").
 
 compile(Template) when is_binary(Template) ->
-    fun (Context) ->
-        iolist_to_binary(sections(Template, set_delimiters(<<"{{ }}">>, #state{contexts = [Context]})))
-    end.
+    tags(Template, #state{}, []).
 
-render(Template, Context) when (is_list(Template) orelse is_binary(Template)) andalso is_map(Context) ->
-    Render = compile(Template),
-    Render(Context);
-render(Render, Context) when is_function(Render, 1), is_map(Context) ->
-    Render(Context).
+tags(<<>>, _State, Acc) -> lists:reverse(Acc);
+tags(Template, #state{left = Left} = State0) ->
+    case re:run(Template, Left) of
+        {match, [{M0, M1} | Type]} ->
 
-sections(<<>>, _State) ->
-    [];
-sections(Template, #state{section_re = SectionRE} = State) ->
-    case re:run(Template, SectionRE) of
-        {match, [{M0, M1}, {K0, K1}, {N0, N1}, {C0, C1}]} ->
             Front = erlang:binary_part(Template, 0, M0),
-            End = M0 + M1,
-            Rest = erlang:binary_part(Template, End, byte_size(Template) - End),
-            Kind = erlang:binary_part(Template, K0, K1),
-            Name = erlang:binary_part(Template, N0, N1),
-            Content = erlang:binary_part(Template, C0, C1),
-            [tags(Front, State),
-             section(Kind, Name, Content, State),
-             sections(Rest, State)];
+            Rest
+            Type = tag_type(Type, Template) of
+                none -> parse_tag(
         nomatch ->
-            tags(Template, State)
+            [Template | Acc]
     end.
 
-section(<<"#">>, Name, Content, #state{contexts = Contexts} = State) ->
-    case get(Name, State) of
-        undefined ->
-            [];
-        {ok, Value} ->
-            case Value of
-                %% TODO: Empty string is not falsy according to the specification
-                False when False =:= false;
-                           False =:= [];
-                           False =:= <<>> ->
-                    [];
-                [Context | _] = SubContexts when is_map(Context) ->
-                    [sections(Content, State#state{contexts = [SubContext | Contexts]})
-                     || SubContext <- SubContexts];
-                List when is_list(List) ->
-                    [sections(Content, State#state{this = Item}) || Item <- List];
-                SubContext when is_map(SubContext) ->
-                    sections(Content, State#state{contexts = [SubContext | Contexts]});
-                Fun when is_function(Fun, 1) ->
-                    sections(Fun(Content), State);
-                Other ->
-                    sections(Content, State#state{this = Other})
-            end
-    end;
-section(<<"^">>, Name, Content, State) ->
-    case get(Name, State) of
-        undefined -> sections(Content, State);
-        %% TODO: Empty string is not falsy according to the specification
-        {ok, False} when False =:= false;
-                         False =:= [];
-                         False =:= <<>> -> sections(Content, State);
-        {ok, _Other} -> []
-    end.
+kind(_Template, []) -> none;
+kind(Template, [{K0, K1}]) ->erlang:binary_part(Template, K0, K1).
 
-tags(Template, #state{tag_re = TagRE} = State0) ->
-    case re:run(Template, TagRE) of
-        {match, [{M0, M1}, K, {C0, C1} | _Rest]} ->
-            Front = erlang:binary_part(Template, 0, M0),
-            End = M0 + M1,
-            Back = erlang:binary_part(Template, End, byte_size(Template) - End),
-            Content = erlang:binary_part(Template, C0, C1),
-            Kind = kind(Template, K),
-            {Result, State1} = tag(Kind, Content, State0),
-            Rest = case Kind of
-                 <<"=">> -> sections(Back, State1);
-                 _       -> tags(Back, State1)
-             end,
-            [Front, Result, Rest];
-        nomatch ->
-            Template
-    end.
+tag(<<"=">>, Content, State)  ->
+    {[], set_delimiters(Content, State)};
+tag(Type, Content, State) ->
+    {{tag, Type, Content}, State}.
 
-kind(_Template, {-1, 0}) -> none;
-kind(Template, {K0, K1}) -> erlang:binary_part(Template, K0, K1).
-
-tag(none, Content, State)     -> {escaped_tag(Content, State), State};
-tag(<<"&">>, Content, State)  -> {unescaped_tag(Content, State), State};
-tag(<<"{">>, Content, State)  -> {unescaped_tag(Content, State), State};
-tag(<<"!">>, _Content, State) -> {[], State};
-tag(<<"=">>, Content, State)  -> {[], set_delimiters(Content, State)}.
-
-escaped_tag(Name, State) ->
-    case get(Name, State) of
-        undefined   -> [];
-        {ok, Value} -> escape(Value)
-    end.
-
-unescaped_tag(Name, State) ->
-    case get(Name, State) of
-        undefined   -> [];
-        {ok, Value} -> to_binary(Value)
-    end.
-
-dot_notation(<<".">>) -> this;
-dot_notation(Content) -> re:split(Content, <<"\\.">>, [unicode]).
-
-get(Name, State) when is_binary(Name) -> get(dot_notation(Name), State);
-get(this, #state{this = This}) -> {ok, This};
-get(Path, #state{contexts = Contexts}) -> context_get(Path, Contexts).
-
-context_get(_Path, []) ->
-    undefined;
-context_get(Path, [Context | Contexts]) ->
-    case get_path(Path, Context) of
-        {ok, Value} -> {ok, Value};
-        undefined   -> context_get(Path, Contexts)
-    end.
-
-get_path([], Value) ->
-    {ok, Value};
-get_path([Key | Keys], Map) when is_map(Map) ->
-    case maps:find(Key, Map) of
-        {ok, Value} -> get_path(Keys, Value);
-        error       -> undefined
-    end.
-
-to_binary(Value) when is_integer(Value) -> integer_to_binary(Value);
-to_binary(Value) when is_float(Value)   -> float_to_binary(Value, [{decimals, 2}]);
-to_binary(Value) when is_atom(Value)    -> atom_to_binary(Value, utf8);
-to_binary(Value) when is_binary(Value)  -> Value.
-
-escape(Binary) when is_binary(Binary) -> escape(unicode:characters_to_list(Binary), []);
-escape(Term)                          -> escape(to_binary(Term)).
-
-escape([],          Acc) -> unicode:characters_to_binary(lists:reverse(Acc));
-escape([$< | Rest], Acc) -> escape(Rest, [<<"&lt;">> | Acc]);
-escape([$> | Rest], Acc) -> escape(Rest, [<<"&gt;">> | Acc]);
-escape([$& | Rest], Acc) -> escape(Rest, [<<"&amp;">> | Acc]);
-escape([X | Rest],  Acc) -> escape(Rest, [X | Acc]).
-
-set_delimiters(Content, State) ->
-    [Left, Right] = re:split(Content, <<" ">>, [{return, list}]),
-    {ok, CompiledSectionRE} = section_re(Left, Right),
-    {ok, CompiledTagRE} = tag_re(Left, Right),
-    State#state{section_re = CompiledSectionRE, tag_re = CompiledTagRE}.
-
-section_re(Left0, Right0) ->
-    {{_, Left1}, {[EndChar | _], Right1}} = escape_delimiters(Left0, Right0),
-    Stop = [<<"([^">>, escape_char(EndChar), <<"]*)">>],
-    Regexp = [Left1, <<"(#|\\^)">>, Stop, Right1, <<"(.+?)">>, Left1, <<"/\\2">>, Right1],
-    re:compile(Regexp, [dotall, unicode]).
-
-tag_re(Left0, Right0) ->
-    {{_,Left1}, {_,Right1}} = escape_delimiters(Left0, Right0),
-    Regexp = [Left1, "(#|=|!|>|{|&)?\\h*(.+?)\\h*(=|})?", Right1],
-    re:compile(Regexp, [dotall, unicode]).
-
-escape_delimiters(Left0, Right0) ->
-    {Left1, Right1} = {unicode:characters_to_list(Left0), unicode:characters_to_list(Right0)},
-    {{Left1, escape_delimiter(Left1)}, {Right1, escape_delimiter(Right1)}}.
-
-escape_delimiter(Delimiter) ->
-    unicode:characters_to_binary(lists:map(fun escape_char/1, Delimiter)).
-
-escape_char(Char) -> [$\\, Char].
+parse_key(<<".">>) -> this;
+parse_key(Content) -> re:split(Content, <<"\\.">>, [unicode]).
--- test/mustache_tests.erl
+++ test/mustache_tests.erl
@@ -27,139 +27,156 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-simple_test() ->
-    test_helper(<<"Hello {{name}}!">>, <<"Hello world!">>, #{<<"name">> => <<"world">>}).
-
-integer_values_too_test() ->
-    test_helper(
-      <<"Hello {{name}}~nYou have just won ${{value}}!">>,
-      <<"Hello Chris~nYou have just won $10000!">>,
-      #{<<"name">> => <<"Chris">>, <<"value">> => 10000}
-    ).
-
-specials_test() ->
-    test_helper(
-        <<"\'Hello\n\"{{name}}\"~nYou \"have\" ju\0st\\ won\b\r\"${{value}}!\"\t">>,
-        <<"\'Hello\n\"Chris\"~nYou \"have\" ju\0st\\ won\b\r\"$10000!\"\t">>,
-        #{<<"name">> => <<"Chris">>, <<"value">> => 10000}
-    ).
-
-binary_test() ->
-    test_helper(
-      <<"Hello {{name}}~nYou have just won ${{value}}!">>,
-      <<"Hello Chris~nYou have just won $10000!">>,
-      #{<<"name">> => <<"Chris">>, <<"value">> => 10000}
-    ).
-
-simple_dot_test() ->
-    test_helper(
-      <<"Hello {{name.first}} {{name.last}}!">>,
-      <<"Hello Emil Falk!">>,
-      #{<<"name">> => #{<<"first">> => <<"Emil">>, <<"last">> => <<"Falk">>}}
-    ).
-
-simple_section_dot_test() ->
-    test_helper(<<"{{#test}}{{.}}{{/test}}">>, <<"123">>, #{<<"test">> => lists:seq(1,3)}).
-
-complex_section_dot_test() ->
-    test_helper(
-        <<"{{#test1.test2}}{{.}}{{/test1.test2}}">>,
-        <<"123">>,
-        #{<<"test1">> => #{<<"test2">> => lists:seq(1, 3)}}
-    ).
-
-object_section_test() ->
-    test_helper(<<"{{#test}}{{name}}{{/test}}">>, <<"Emil">>, #{<<"test">> => #{<<"name">> => <<"Emil">>}}).
-
-fun_test() ->
-    test_helper(
-      <<"{{#test1}}xxx{{/test1}}">>,
-      <<"yyy">>,
-      #{<<"test1">> => fun (Text) -> re:replace(Text, <<"xxx">>, <<"{{test2}}">>, [{return, binary}]) end,
-        <<"test2">> => <<"yyy">>}
-    ).
-
-set_delimiter_test() ->
-    test_helper(
-        <<"{{=[[ ]]=}}[[{test1}]] [[={{ }}=]]{{{test2}}}">>,
-        <<"TEST1 TEST2">>,
-        #{<<"test1">> => <<"TEST1">>, <<"test2">> => <<"TEST2">>}
-    ).
-
-set_delimiter_boolean_section_test() ->
-    test_helper(
-        <<"{{=[[ ]]=}}[[#test1.test2]]TEST[[/test1.test2]] {{::test}}">>,
-        <<"TEST {{::test}}">>,
-        #{<<"test1">> => #{<<"test2">> => true}}
-    ).
-
-%% ===================================================================
-%% basic tag types
-%% ===================================================================
-
-tag_type_variable_empty_test() ->
-    test_helper(<<"{{name}}">>, <<>>, #{}).
-
-tag_type_variable_string_test() ->
-    test_helper(<<"{{name}}">>, <<"NAME">>, #{<<"name">> => <<"NAME">>}).
-
-tag_type_variable_integer_test() ->
-    test_helper(<<"{{name}}">>, <<"1">>, #{<<"name">> => 1}).
-
-tag_type_variable_boolean_test() ->
-    test_helper(<<"{{name}}">>, <<"true">>, #{<<"name">> => true}).
-
-tag_type_varibale_escaped_test() ->
-    test_helper(<<"{{name}}">>, <<"&gt;&amp;do&lt;it&gt;">>, #{<<"name">> => <<">&do<it>">>}).
-
-tag_type_variabel_unescaped_test() ->
-    test_helper(<<"{{{name}}}">>, <<">dont&do<it>">>, #{<<"name">> => <<">dont&do<it>">>}).
-
-tag_type_variable_unescaped_with_ampersand_test() ->
-    test_helper(<<"{{&name}}">>, <<">dont&do<it>">>, #{<<"name">> => <<">dont&do<it>">>}).
-
-tag_type_section_empty_test() ->
-    test_helper(<<"{{#name}}section{{/name}}">>, <<>>, #{}).
-
-tag_type_section_false_test() ->
-    test_helper(<<"{{#name}}section{{/name}}">>, <<>>, #{<<"name">> => false}).
-
-tag_type_section_true_test() ->
-    test_helper(<<"{{#name}}section{{/name}}">>, <<"section">>, #{<<"name">> => true}).
-
-tag_type_section_empty_list_test() ->
-    test_helper(<<"{{#name}}section{{/name}}">>, <<>>, #{<<"name">> => []}).
-
-tag_type_section_nonempty_list_test() ->
-    CtxList = #{<<"name">> => [ #{} || _ <- lists:seq(1,3) ]},
-    test_helper(<<"{{#name}}section{{/name}}">>, <<"sectionsectionsection">>, CtxList).
-
-tag_type_inverted_section_empty_test() ->
-    test_helper(<<"{{^name}}section{{/name}}">>, <<"section">>, #{}).
-
-tag_type_inverted_section_false_test() ->
-    test_helper(<<"{{^name}}section{{/name}}">>, <<"section">>, #{<<"name">> => false}).
-
-tag_type_inverted_section_true_test() ->
-    test_helper(<<"{{^name}}section{{/name}}">>, <<>>, #{<<"name">> => true}).
-
-tag_type_inverted_section_empty_list_test() ->
-    test_helper(<<"{{^name}}section{{/name}}">>, <<"section">>, #{<<"name">> => []}).
-
-tag_type_inverted_section_nonempty_list_test() ->
-    CtxList = #{<<"name">> => [ #{} || _ <- lists:seq(1,3)]},
-    test_helper(<<"{{^name}}section{{/name}}">>, <<>>, CtxList).
-
-
-tag_type_comment_test() ->
-    test_helper(<<"{{!comment}}">>, <<>>, #{}).
-
-tag_type_comment_empty_test() ->
-    test_helper(<<"{{! }}">>, <<>>, #{}).
-
-tag_type_comment_multiline_test() ->
-    test_helper(<<"{{!\ncomment\ncomment\ncomment\n\n}}">>, <<>>, #{}).
-
-test_helper(Template, Expected, Ctx) ->
-    Result = iolist_to_binary(mustache:render(Template, Ctx)),
+compile_test() ->
+    Template = <<"1{{a}}2{{{b}}}3{{#c}}4{{#d}}xxx{{/d}}5{{/c}}6{{^e}}yyy{{/e}}7{{& f}}8{{! apa}}9">>,
+    Expected = [<<"1">>,
+                {tag, escaped, [<<"a">>]},
+                <<"2">>,
+                {tag, unescaped, [<<"b">>]},
+                {section, normal, [<<"c">>], [<<"4">>, {section, normal, [<<"d">>], [<<"xxx">>]}, <<"5">>]},
+                <<"6">>,
+                {section, inverted, [<<"e">>], [<<"yyy">>]},
+                <<"7">>,
+                {tag, unescaped, [<<"f">>]},
+                <<"8">>,
+                <<"9">>],
+    Result = mustache:compile(Template),
+    ?debugFmt("Sections: ~p~n~n", [Result]),
     ?assertEqual(Expected, Result).
+
+%simple_test() ->
+%    test_helper(<<"Hello {{name}}!">>, <<"Hello world!">>, #{<<"name">> => <<"world">>}).
+%
+%integer_values_too_test() ->
+%    test_helper(
+%      <<"Hello {{name}}~nYou have just won ${{value}}!">>,
+%      <<"Hello Chris~nYou have just won $10000!">>,
+%      #{<<"name">> => <<"Chris">>, <<"value">> => 10000}
+%    ).
+%
+%specials_test() ->
+%    test_helper(
+%        <<"\'Hello\n\"{{name}}\"~nYou \"have\" ju\0st\\ won\b\r\"${{value}}!\"\t">>,
+%        <<"\'Hello\n\"Chris\"~nYou \"have\" ju\0st\\ won\b\r\"$10000!\"\t">>,
+%        #{<<"name">> => <<"Chris">>, <<"value">> => 10000}
+%    ).
+%
+%binary_test() ->
+%    test_helper(
+%      <<"Hello {{name}}~nYou have just won ${{value}}!">>,
+%      <<"Hello Chris~nYou have just won $10000!">>,
+%      #{<<"name">> => <<"Chris">>, <<"value">> => 10000}
+%    ).
+%
+%simple_dot_test() ->
+%    test_helper(
+%      <<"Hello {{name.first}} {{name.last}}!">>,
+%      <<"Hello Emil Falk!">>,
+%      #{<<"name">> => #{<<"first">> => <<"Emil">>, <<"last">> => <<"Falk">>}}
+%    ).
+%
+%simple_section_dot_test() ->
+%    test_helper(<<"{{#test}}{{.}}{{/test}}">>, <<"123">>, #{<<"test">> => lists:seq(1,3)}).
+%
+%complex_section_dot_test() ->
+%    test_helper(
+%        <<"{{#test1.test2}}{{.}}{{/test1.test2}}">>,
+%        <<"123">>,
+%        #{<<"test1">> => #{<<"test2">> => lists:seq(1, 3)}}
+%    ).
+%
+%object_section_test() ->
+%    test_helper(<<"{{#test}}{{name}}{{/test}}">>, <<"Emil">>, #{<<"test">> => #{<<"name">> => <<"Emil">>}}).
+%
+%fun_test() ->
+%    test_helper(
+%      <<"{{#test1}}xxx{{/test1}}">>,
+%      <<"yyy">>,
+%      #{<<"test1">> => fun (Text) -> re:replace(Text, <<"xxx">>, <<"{{test2}}">>, [{return, binary}]) end,
+%        <<"test2">> => <<"yyy">>}
+%    ).
+%
+%set_delimiter_test() ->
+%    test_helper(
+%        <<"{{=[[ ]]=}}[[{test1}]] [[={{ }}=]]{{{test2}}}">>,
+%        <<"TEST1 TEST2">>,
+%        #{<<"test1">> => <<"TEST1">>, <<"test2">> => <<"TEST2">>}
+%    ).
+%
+%set_delimiter_boolean_section_test() ->
+%    test_helper(
+%        <<"{{=[[ ]]=}}[[#test1.test2]]TEST[[/test1.test2]] {{::test}}">>,
+%        <<"TEST {{::test}}">>,
+%        #{<<"test1">> => #{<<"test2">> => true}}
+%    ).
+%
+%%% ===================================================================
+%%% basic tag types
+%%% ===================================================================
+%
+%tag_type_variable_empty_test() ->
+%    test_helper(<<"{{name}}">>, <<>>, #{}).
+%
+%tag_type_variable_string_test() ->
+%    test_helper(<<"{{name}}">>, <<"NAME">>, #{<<"name">> => <<"NAME">>}).
+%
+%tag_type_variable_integer_test() ->
+%    test_helper(<<"{{name}}">>, <<"1">>, #{<<"name">> => 1}).
+%
+%tag_type_variable_boolean_test() ->
+%    test_helper(<<"{{name}}">>, <<"true">>, #{<<"name">> => true}).
+%
+%tag_type_varibale_escaped_test() ->
+%    test_helper(<<"{{name}}">>, <<"&gt;&amp;do&lt;it&gt;">>, #{<<"name">> => <<">&do<it>">>}).
+%
+%tag_type_variabel_unescaped_test() ->
+%    test_helper(<<"{{{name}}}">>, <<">dont&do<it>">>, #{<<"name">> => <<">dont&do<it>">>}).
+%
+%tag_type_variable_unescaped_with_ampersand_test() ->
+%    test_helper(<<"{{&name}}">>, <<">dont&do<it>">>, #{<<"name">> => <<">dont&do<it>">>}).
+%
+%tag_type_section_empty_test() ->
+%    test_helper(<<"{{#name}}section{{/name}}">>, <<>>, #{}).
+%
+%tag_type_section_false_test() ->
+%    test_helper(<<"{{#name}}section{{/name}}">>, <<>>, #{<<"name">> => false}).
+%
+%tag_type_section_true_test() ->
+%    test_helper(<<"{{#name}}section{{/name}}">>, <<"section">>, #{<<"name">> => true}).
+%
+%tag_type_section_empty_list_test() ->
+%    test_helper(<<"{{#name}}section{{/name}}">>, <<>>, #{<<"name">> => []}).
+%
+%tag_type_section_nonempty_list_test() ->
+%    CtxList = #{<<"name">> => [ #{} || _ <- lists:seq(1,3) ]},
+%    test_helper(<<"{{#name}}section{{/name}}">>, <<"sectionsectionsection">>, CtxList).
+%
+%tag_type_inverted_section_empty_test() ->
+%    test_helper(<<"{{^name}}section{{/name}}">>, <<"section">>, #{}).
+%
+%tag_type_inverted_section_false_test() ->
+%    test_helper(<<"{{^name}}section{{/name}}">>, <<"section">>, #{<<"name">> => false}).
+%
+%tag_type_inverted_section_true_test() ->
+%    test_helper(<<"{{^name}}section{{/name}}">>, <<>>, #{<<"name">> => true}).
+%
+%tag_type_inverted_section_empty_list_test() ->
+%    test_helper(<<"{{^name}}section{{/name}}">>, <<"section">>, #{<<"name">> => []}).
+%
+%tag_type_inverted_section_nonempty_list_test() ->
+%    CtxList = #{<<"name">> => [ #{} || _ <- lists:seq(1,3)]},
+%    test_helper(<<"{{^name}}section{{/name}}">>, <<>>, CtxList).
+%
+%
+%tag_type_comment_test() ->
+%    test_helper(<<"{{!comment}}">>, <<>>, #{}).
+%
+%tag_type_comment_empty_test() ->
+%    test_helper(<<"{{! }}">>, <<>>, #{}).
+%
+%tag_type_comment_multiline_test() ->
+%    test_helper(<<"{{!\ncomment\ncomment\ncomment\n\n}}">>, <<>>, #{}).
+%
+%test_helper(Template, Expected, Ctx) ->
+%    Result = iolist_to_binary(mustache:render(Template, Ctx)),
+%    ?assertEqual(Expected, Result).
